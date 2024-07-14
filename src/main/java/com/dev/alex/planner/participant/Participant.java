bec7c20ad1b54d448a4c75c6434ddbe6fc575acd
Alexandre Gaspar <alexandretiao16@gmail.com>
2024-07-14 14:09:05 +0100

    ⚡️ Feat: create entity participant
    Signed-off-by: Alexandre Gaspar <alexandretiao16@gmail.com>

---
+++ src/main/java/com/dev/alex/planner/participant/Participant.java
@@ -0,0 +1,35 @@
+package com.dev.alex.planner.participant;
+
+import com.dev.alex.planner.trip.Trip;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "participants")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Participant {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(name = "is_confirmed", nullable = false)
+    private Boolean isConfirmed;
+
+    @ManyToOne
+    @JoinColumn(name = "trip_id", nullable = false)
+    private Trip trip;
+
+}
